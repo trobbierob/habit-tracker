af66646a39ce7e318380eb8750d814fcee955382
trobbierob <troberts@my.hpu.edu>
2017-02-20 21:08:38 -0500

    Set Colors and Add FAB
    Create FAB and Intent for HabitCategory class.

--- app/src/main/java/com/example/android/habittracker/MainActivity.java
+++ app/src/main/java/com/example/android/habittracker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FAB will open Habit Category activity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HabitCategory.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
